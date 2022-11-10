3b26317169f7d77302653ffa181383e665d445bd
DemetrioG <demetrio.gabriel@hotmail.com>
2022-11-10 20:38:15 -0300

    feat: teste de retorno dynamodb

--- src/utils/returnData.ts
+++ src/utils/returnData.ts
@@ -1,7 +1,30 @@
+import AWS from "aws-sdk";
+
+import { CONFIG } from "../config";
+
+AWS.config.update(CONFIG.aws);
+const DYNAMODB = new AWS.DynamoDB.DocumentClient();
+
 const returnData = async () => {
-  return {
-    status: "ok",
-  };
+  return new Promise((resolve, reject) => {
+    try {
+      const params = {
+        TableName: "dynamo-scala",
+        Key: {
+          id: 1,
+          name: "test1",
+        },
+      };
+
+      DYNAMODB.get(params, (err, data) => {
+        if (!err) {
+          resolve(data.Item);
+        }
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
 
 export default returnData;
